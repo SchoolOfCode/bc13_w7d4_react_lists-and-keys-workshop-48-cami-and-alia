814e32199fcfa68835b44433e17bb427de3a6d1b
aliafarhana88 <alia.abdulghaffar@gmail.com>
2022-11-10 16:31:13 +0000

    Task 5 done

--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -2,8 +2,30 @@ import React from "react";
 
 import "./index.css";
 
+//PLAN for Image 
+//  image: { link: 'link', alt: 'alt' }
+//<img src = "link" alt = "alt">
+//image.link   image.alt 
+
+
 function Post({ title, date, author, text, highlights, image }) {
-  return <article>{text}</article>;
+  return <article>
+  <h2>{title}</h2>
+  <h3>{date}</h3>
+  <h3>{author}</h3>
+  <p>{text}</p>
+ 
+  {highlights.map(highlight=>{
+    //const keyID = highlights.findIndex(index=>index===highlight)
+    const keyID = highlights.indexOf(highlight)
+    console.log(keyID)
+    return <li>{highlight}</li>
+  })}
+
+  <img src = {image.link}  alt = {image.alt}/>
+
+
+  </article>;
 }
 
 export default Post;
